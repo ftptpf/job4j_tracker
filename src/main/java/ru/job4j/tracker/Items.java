41fac0b40f52758a73af974e2ad282a7a89edeb6
ftptpf <Nb49Rtdd>
2020-06-02 19:35:07 +0300

    Sample

---
+++ src/main/java/ru/job4j/tracker/Items.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Items {
+    public Items() {
+        super();
+        System.out.println("load item");
+    }
+    public static void main(String[] args) {
+        Bug bug = new Bug();
+    }
+}
