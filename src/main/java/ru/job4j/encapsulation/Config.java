58e288339d2fdd886a855f43f204da3fe9b2d1e8
ftptpf <Nb49Rtdd>
2020-06-12 19:14:37 +0300

    0. Что такое инкапсуляция. [#283920]

---
+++ src/main/java/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,19 @@
+package ru.job4j.encapsulation;
+
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+    public Config(String name) {
+        this.name = name;
+    }
+    void print() {
+        System.out.println(position);
+    }
+    public String getProperty(String key) {
+        return key;
+    }
+    public String search(String key) {
+        return key;
+    }
+}
