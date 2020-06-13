a5676f8725c376ff5c9bd21d4eee30353b968fcc
ftptpf <Nb49Rtdd>
2020-06-13 14:05:51 +0300

    2. Перегрузка конструктора. - this [#283906]

--- src/main/java/ru/job4j/oop/Animal.java
+++ src/main/java/ru/job4j/oop/Animal.java
@@ -1,12 +1,14 @@
 package ru.job4j.oop;
 
 public class Animal {
+    private String name;
     public Animal() {
         super();
         System.out.println("Load item 1");
     }
     public Animal(String name) {
         super();
+        this.name = name;
         System.out.println("Animal " + name);
     }
     public static void main(String[] args) {
