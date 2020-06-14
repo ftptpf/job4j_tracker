caf88e65715d1b748347a048ca7f8e73a2761fcc
ftptpf <Nb49Rtdd>
2020-06-14 21:27:05 +0300

    3. Удаление моделей из массива. - delete [#283915]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -1,6 +1,14 @@
 package ru.job4j.pojo;
 
 public class Shop {
+    public Product[] delete(Product[] products) {
+        for (int index = 0; index < products.length - 1; index++) {
+            products[index] = products[index + 1];
+        }
+        products[products.length - 1] = null;
+        return products;
+    }
+
     public static void main(String[] args) {
         Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);
@@ -9,7 +17,7 @@ public class Shop {
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
-            if(product != null) {
+            if (product != null) {
                 System.out.println(product.getName());
             } else {
                 System.out.println("null");
@@ -21,25 +29,42 @@ public class Shop {
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
-            if(product != null) {
+            if (product != null) {
                 System.out.println(product.getName());
             } else {
                 System.out.println("null");
             }
         }
         System.out.println();
-        System.out.println("Записываем в ячейку с индексом 1 значение с индексом 2. " +
-                "И удаляем значение из ячейки с индексом 2.");
-        products[1] = products[2];
-        products[2] = null;
+        System.out.println("Проверяем работу метода delete. Смещение всего массива влево и назначение крайнему правому" +
+                "элементу массива значения null.");
 
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
-            if(product != null) {
+            Shop move = new Shop();
+            move.delete(products);
+
+            if (product != null) {
                 System.out.println(product.getName());
             } else {
                 System.out.println("null");
             }
         }
+
+            System.out.println();
+            System.out.println("Записываем в ячейку с индексом 1 значение с индексом 2. " +
+                    "И удаляем значение из ячейки с индексом 2.");
+            products[1] = products[2];
+            products[2] = null;
+
+            for (int i = 0; i < products.length; i++) {
+                Product product = products[i];
+                if (product != null) {
+                    System.out.println(product.getName());
+                } else {
+                    System.out.println("null");
+                }
+            }
+        }
     }
-}
+
