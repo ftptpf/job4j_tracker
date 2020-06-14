647059c89131c4034f6888c7d1e5de72b6b8b71f
ftptpf <Nb49Rtdd>
2020-06-14 21:37:59 +0300

    4. Сравнение моделей. Метод equals - equals [#283914]

--- src/main/java/ru/job4j/pojo/ProductContains.java
+++ src/main/java/ru/job4j/pojo/ProductContains.java
@@ -4,7 +4,7 @@ public class ProductContains {
     public static void main(String[] args) {
         Product first = new Product("Milk", 100);
         Product second = new Product("Milk", 100);
-        boolean eq = first == second;
+        boolean eq = first.equals(second);
         System.out.println(eq);
     }
 }
