58c6a2dbc7a6bdcecee7f65ff74421c28136ed97
ftptpf <Nb49Rtdd>
2020-07-05 21:29:56 +0300

    2.1. Реализация класса StartUI [#283862]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,10 +1,81 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.print("Select:");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine(); //в командной строке вводим имя
+                Item item = new Item();
+                item.setName(name); // записываем имя "Item"
+                tracker.add(item); // в массива "items" записываем имя и автоматически присваиваем id
+            } else if (select == 1) {
+                System.out.println("=== All items ====");
+                Item[] item = tracker.findAll(); // должен показать все не пустые элементы массива //**** с этим проблема ***// ((
+                for (int i = 0; i < item.length; i++) { //цикл для поэлементного вывода значений массива
+                    System.out.println(item[i]);
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit (replace) item ====");
+                System.out.print("Enter id: ");
+                int id = Integer.valueOf(scanner.nextLine()); // вводим в командной строке id который ищем
+                Item current = tracker.findById(id); // по id находим текущее значение Item и присваиваем его current
+                System.out.println(current); // выводим значение current в консоль
+                System.out.print("Enter new Name: ");
+                String name = scanner.nextLine(); // вводим новое имя в командной строке для "Item"
+                Item newItem = new Item();
+                newItem.setName(name); // записываем новое имя которое ввели в командной строке
+                if (tracker.replace(id, newItem)) { // производим поиск по id c заменой на новое имя в "Item"
+                    System.out.println("Replacement was done.");
+                } else {
+                    System.out.println("Replacement error.");
+                }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ====");
+                System.out.print("Enter id: ");
+                int id = Integer.valueOf(scanner.nextLine()); // вводим в командной строке id по котрому будет найдена
+                // и удалена информация соответствующей ячейки массива
+                if (tracker.delete(id)) { // производим поиск по id и удаление соответстующего "Item" из массива
+                    System.out.println("Delete is Ok.");
+                } else {
+                    System.out.println("Delete error.");
+                }
+            } else if (select == 4) {
+                System.out.println("=== Find item by id ====");
+                System.out.print("Enter id: ");
+                int id = Integer.valueOf(scanner.nextLine()); // вводим в командной строке id который ищем
+                tracker.findById(id);
+            } else if (select == 5) {
+                System.out.println("=== Find item by name ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine(); //в командной строке вводим имя которое ищем
+                tracker.findByName(name);
+            } else  if (select == 6) {
+                run = false;
+            }
+        }
+    }
+    private void showMenu() {
+        System.out.println("Menu.");
+        System.out.println("0. Add new Item.");
+        System.out.println("1. Show all items.");
+        System.out.println("2. Edit item.");
+        System.out.println("3. Delete item.");
+        System.out.println("4. Find item by Id.");
+        System.out.println("5. Find items by name.");
+        System.out.println("6. Exit Program.");
+    }
+
     public static void main(String[] args) {
-        Tracker tr = new Tracker();
-        Item str = new Item();
-        tr.add(str);
-        System.out.println(tr.findById(23));
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner,tracker);
     }
 }
