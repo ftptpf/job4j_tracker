52277241b593acb1cb50bfcf4b0b39f01738934d
ftptpf <Nb49Rtdd>
2020-07-12 20:40:04 +0300

    5.1. Тестирование. Подготовка данных. [#283860]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -18,4 +18,29 @@ public class StartUITest {
         expected.setName("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item();
+        item.setName("some name");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "replace item"};
+        StartUI.replaceItem(new StubInput(answer), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replace item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker(); // Создаем объект Tracker
+        Item item = new Item(); // Создаем объект Item
+        item.setName("some name"); // Записываем в Item параметр name
+        tracker.add(item); // Добавляем Item в Tracker
+        String[] answer = {String.valueOf(item.getId()), "some item"}; // В строковый массив записываем значение
+        StartUI.deleteItem(new StubInput(answer), tracker); // Иммитируем пользовательский ввод с
+        // с переданными в input параметрами строкового массива answer
+        Item delete = tracker.findById(item.getId()); // Запускаем поиск удаленного Item по id
+        assertThat(delete, is("null")); // Сравниваем результат и ожидание
+    }
 }
