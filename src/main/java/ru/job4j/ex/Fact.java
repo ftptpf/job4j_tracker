bf3532dd4b2df01a0b6c9c39f43218034870b12c
ftptpf <Nb49Rtdd>
2020-07-25 18:58:14 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#283870]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+
+
+    }
+
+    public int calc (int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be more then zero");
+        }
+        int rls = 1;
+        for (int index = 0; index < n; index++) {
+            rls *= index;
+        }
+        return rls;
+    }
+}
