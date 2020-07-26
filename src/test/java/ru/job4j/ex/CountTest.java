8ae253d5fcab232992a88eee99e2ddb0fbb8b6d5
ftptpf <Nb49Rtdd>
2020-07-26 11:09:04 +0300

    0.3. Тестирование исключений с junit [#283871]

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class CountTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Count.add(10,2);
+    }
+
+    @Test
+    public void when0to2then3() {
+        int rls = Count.add(0,3);
+        assertThat(rls, is(3));
+    }
+}
