df0e9b24207fa72aa9cc895bd72da596a3e1129d
ftptpf <Nb49Rtdd>
2020-08-02 17:19:22 +0300

    3. Замена if-else-throw на if-throw [#283868] - ArrayIndexOutOfBoundsException

--- src/main/java/ru/job4j/ex/Find.java
+++ src/main/java/ru/job4j/ex/Find.java
@@ -2,15 +2,15 @@ package ru.job4j.ex;
 
 public class Find {
     public static String get(String[] data, int index) {
-        if (index <= 0 && index > data.length) {
-            throw new IllegalArgumentException("Index out of bound");
+        if (index < 0 || index > data.length) {
+            throw new ArrayIndexOutOfBoundsException("Index out of bound");
         }
         return data[index];
     }
 
     public static void main(String[] args) {
         String[] data = {"one", "two", "three"};
-        String rls = Find.get(data, 1);
+        String rls = Find.get(data, 4);
         System.out.println(rls);
     }
 }
