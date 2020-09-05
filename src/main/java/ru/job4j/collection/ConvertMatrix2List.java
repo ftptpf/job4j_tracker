f15641bff776030910395b4245f210e49b06440e
ftptpf <Nb49Rtdd>
2020-09-05 13:54:45 +0300

    4. Конвертация двумерного массива в ArrayList [#283832]

---
+++ src/main/java/ru/job4j/collection/ConvertMatrix2List.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertMatrix2List {
+    /**
+     * Метод реализует конвертацию двухмерного массива в одномерный список.
+     * @param array двухмерный числовой массив int
+     * @return list одномерный список
+     */
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+            for (int[] row : array) {
+                for (int cell : row) {
+                    list.add(cell);
+                }
+            }
+        return list;
+    }
+}
