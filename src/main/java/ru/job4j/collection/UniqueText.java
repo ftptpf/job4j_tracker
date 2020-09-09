a6ee6df03d52c7459695b3cb9de6b446b64230a1
ftptpf <Nb49Rtdd>
2020-09-09 20:33:11 +0300

    2. Идентичные тексты. [#283844]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    /**
+     * Метод сравнивает два текста, проверяет их на уникальность,
+     * т.е. на то что предложения могут содержать одни и теже слова,
+     * но в разной последовательности.
+     * @param originText первый текст (оригинал)
+     * @param duplicateText второй текс (дубликат)
+     * @return
+     */
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");//Преобразование строки в массив слов
+        String[] text = duplicateText.split(" ");//Преобразование строки в массив слов
+        HashSet<String> check = new HashSet<>();
+        for (String str : origin) {// Проходим по массиву, берем слова из (оригинального) текста
+            check.add(str);//вставляем (уникальные) уникальные слова в коллекцию
+        }
+        for (String hs : text) {// Проходим по массиву, берем слова из (проверяемого) текста
+            if (check.contains(hs) == false) {//проверяем, если хотя бы одно из слов уникально, его нет в оригинальной строке
+                rsl = false; // возврашаем false
+            }
+        }
+        return rsl;
+    }
+}
