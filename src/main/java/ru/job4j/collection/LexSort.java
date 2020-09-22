4c25d4d49f12348ea0a0d61db54e4064cbda5ff6
ftptpf <Nb49Rtdd>
2020-09-22 22:07:16 +0300

    4. Сортировка номера [#283825]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        String[] lf = left.split("\\.", 2); //делим строку left по разделителю "." в массив из двух состставляющих
+        String[] rf =  right.split("\\.", 2);//делим строку right по разделителю "." в массив из двух состставляющих
+
+        int lint = Integer.parseInt(lf[0]);//строку в массиве по индексу [0] преобразуем в число
+        int rint = Integer.parseInt(rf[0]);//строку в массиве по индексу [0] преобразуем в число
+
+        if (lint > rint) {
+            result = 1;
+        } else if (lint < rint) {
+            result = -1;
+        }
+        return result;
+    }
+}
