69f08165e8256a0094e53911def99c53c61f70dc
ftptpf <Nb49Rtdd>
2020-09-27 23:44:31 +0300

    1. JaCoCo. Процент покрытие тестами. [#283817] - target

--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -21,7 +21,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("34", "5546").getBalance(), is(150D));
+        assertNull(bank.findByRequisite("34", "5546"));
     }
 
     @Test
