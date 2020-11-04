ac3db3a8b724af34fd6259787bf197b3aacc8948
ftptpf <Nb49Rtdd>
2020-11-04 22:23:31 +0300

    1. Фильтрация учеников. [#283694]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class School {
+    /**
+     * Метод делит учеников на три группы по общему баллу score:
+     * score - может быть в диапазоне от 0 до 100.
+     * 10А диапазон балла [70: 100],
+     * 10Б диапазон балла [50: 70);
+     * 10B диапазон балла (0: 50);
+     * @param students
+     * @param predicate
+     * @return
+     */
+    public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
+        List<Student> list = new ArrayList<>();
+        list = students.stream().filter(predicate).collect(Collectors.toList());
+        return list;
+    }
+}
