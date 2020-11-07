8be71bce0893ba426cd0bacb4674eb07d1c2f9e5
ftptpf <Nb49Rtdd>
2020-11-07 15:39:41 +0300

    2. Список адресов [#283695]

---
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profiles {
+    /**
+     * Метод составляет список адресов клиентов.
+     * @param profiles
+     * @return
+     */
+    List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().map(x -> x.getAddress()).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    private List<Address> list = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        list.add(new Address("Moscow", "Red", 1, 3));
+        list.add(new Address("Samara", "Long", 11, 36));
+
+    }
+    @Test
+    public void collect() {
+        List<Address> rsl = new ArrayList<>();
+        List<Profile> pr = new ArrayList<>();
+        pr.add(new Profile(new Address("Moscow", "Red", 1, 3)));
+        pr.add(new Profile(new Address("Samara", "Long", 11, 36)));
+        Profiles r = new Profiles();
+        rsl = r.collect(pr);
+        assertThat(rsl, is(list));
+
+
+
+
+    }
+}
