ca6b8e8d2a1b8b40dc8a0b7df9670a2dd7ae5f11
ftptpf <inboxru@yandex.ru>
2021-05-28 09:47:02 +0300

    2. Трекер SQL [#1734] - PreparedStatement

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,8 +1,8 @@
 package ru.job4j.tracker;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -32,22 +32,63 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        return null;
+        try (PreparedStatement pStatement = cn.prepareStatement("INSERT INTO items (name) VALUES (?)",
+                Statement.RETURN_GENERATED_KEYS)) {
+            pStatement.setString(1, item.getName());
+            pStatement.execute();
+            try (ResultSet getGeneratedKey = pStatement.getGeneratedKeys()) {
+                if (getGeneratedKey.next()) {
+                    item.setId(getGeneratedKey.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        return false;
+        boolean result = false;
+        try (PreparedStatement pdStatement = cn.prepareStatement("UPDATE items SET name = ? WHERE id = ?")) {
+            pdStatement.setString(1, item.getName());
+            pdStatement.setInt(2, id);
+            result = pdStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        boolean result = false;
+        try (PreparedStatement pStatement = cn.prepareStatement("DELETE FROM items WHERE id = ?")) {
+            pStatement.setInt(1, id);
+            result = pStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement pStatement = cn.prepareStatement("SELECT * FROM items")) {
+            try (ResultSet resultSet = pStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    items.add(new Item(
+                            resultSet.getInt("id"),
+                            resultSet.getString("name")
+                    ));
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
     }
 
     @Override
