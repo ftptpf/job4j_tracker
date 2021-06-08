d295406f01d1793ff05cadaa99ea64afe5d0e0a1
ftptpf <Nb49Rtdd>
2021-06-08 22:43:51 +0300

    4.4. Liquibase. Интеграционные тесты. Proxy. [#95881] - StartUITest

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,17 +7,14 @@ import ru.job4j.tracker.menu.*;
 import ru.job4j.tracker.output.Output;
 import ru.job4j.tracker.output.other.StubOutput;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 
@@ -44,19 +41,6 @@ public class StartUITest {
 
     @Test
     public void whenAddItem() throws Exception {
-/*        Output out = new StubOutput();
-        Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
-        );
-        Store tracker = new SqlTracker();
-
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new CreateAction(out));
-        actions.add(new ExitProgramAction(out));
-
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findAll().get(0).getName(), is("Item name"));*/
-        //SqlTracker tracker = new SqlTracker();
         try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
             Item item = new Item();
             item.setName(nameOne);
@@ -67,25 +51,6 @@ public class StartUITest {
 
     @Test
     public void whenReplaceItem() throws Exception {
-/*        Output out = new StubOutput();
-        Store tracker = new SqlTracker();
-        Item item = new Item();
-        item.setName("Replaced item");
-        tracker.add(item);
-        String replacedName = "New item name";
-        String nameNew = item.getId() + " " + replacedName;
-        Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), "New item name", "1"}
-        );
-
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new ReplaceAction(out));
-        actions.add(new ExitProgramAction(out));
-
-        new StartUI(out).init(in, tracker, actions);
-        String nameNewResult = String.valueOf(tracker.findById(item.getId()));
-        assertThat(nameNewResult, is(nameNew));*/
-
         try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
             Item itemOne = new Item();
             itemOne.setName(nameOne);
@@ -101,21 +66,6 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItem() throws Exception {
-/*        Output out = new StubOutput();
-        Store tracker = new SqlTracker();
-        Item item = new Item();
-        item.setName("Deleted item");
-        tracker.add(item);
-        Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), "1"}
-        );
-
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new DeleteAction(out));
-        actions.add(new ExitProgramAction(out));
-
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()), is(nullValue()));*/
         try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
             Item itemOne = new Item();
             itemOne.setName(nameOne);
@@ -145,33 +95,6 @@ public class StartUITest {
 
     @Test
     public void whenFindAll() throws Exception {
-/*        Output out = new StubOutput();
-        Store tracker = new SqlTracker();
-        Item item1 = new Item();
-        item1.setName("One");
-        tracker.add(item1);
-        Item item2 = new Item();
-        item2.setName("Two");
-        tracker.add(item2);
-        Input in = new StubInput(
-                new String[] {"0", "1"}
-        );
-
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new FindAllAction(out));
-        actions.add(new ExitProgramAction(out));
-
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                "Menu." + br
-                        + "0. Show all items." + br
-                        + "1. Exit Program." + br
-                        + "=== All Items ===" + br
-                        + "1 One" + br
-                        + "2 Two" + br
-                        + "Menu." + br
-                        + "0. Show all items." + br
-                        + "1. Exit Program." + br));*/
         try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
             Item itemOne = new Item();
             itemOne.setName(nameOne);
@@ -192,65 +115,32 @@ public class StartUITest {
     }
 
     @Test
-    public void whenFindById() {
-/*        Output out = new StubOutput();
-        Store tracker = new SqlTracker();
-        Item item1 = new Item();
-        item1.setName("One");
-        tracker.add(item1);
-        Item item2 = new Item();
-        item2.setName("Two");
-        tracker.add(item2);
-        Input in = new StubInput(
-                new String[] {"0", String.valueOf(item1.getId()), "1"}
-        );
-
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new FindAllByIdAction(out));
-        actions.add(new ExitProgramAction(out));
-
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                "Menu." + br
-                        + "0. Find item by Id." + br
-                        + "1. Exit Program." + br
-                        + "=== Find item by id ===" + br
-                        + "1 One" + br
-                        + "Menu." + br
-                        + "0. Find item by Id." + br
-                        + "1. Exit Program." + br));*/
-
+    public void whenFindById() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item itemOne = new Item();
+            itemOne.setName(nameOne);
+            tracker.add(itemOne);
+            assertThat(tracker.findById(itemOne.getId()), is(itemOne));
+        }
     }
 
     @Test
-    public void whenFindByName() {
-/*        Output out = new StubOutput();
-        Store tracker = new SqlTracker();
-        Item item1 = new Item();
-        item1.setName("One");
-        tracker.add(item1);
-        Item item2 = new Item();
-        item2.setName("Two");
-        tracker.add(item2);
-        Input in = new StubInput(
-                new String[]{"0", String.valueOf(item1.getName()), "1"}
-        );
+    public void whenFindByName() throws Exception {
+        try (SqlTracker tracker = new SqlTracker(ConnectionRollback.create(this.init()))) {
+            Item itemOne = new Item();
+            itemOne.setName(nameOne);
+            tracker.add(itemOne);
 
-        List<UserAction> actions = new ArrayList<>();
-        actions.add(new FindByNameAction(out));
-        actions.add(new ExitProgramAction(out));
+            Item itemTwo = new Item();
+            itemTwo.setName(nameTwo);
+            tracker.add(itemTwo);
+            assertThat(tracker.findByName(nameOne).size(), is(1));
 
-        new StartUI(out).init(in, tracker, actions);
-        assertThat(out.toString(), is(
-                "Menu." + br
-                        + "0. Find items by name." + br
-                        + "1. Exit Program." + br
-                        + "=== Find item by name ===" + br
-                        + "1 One" + br
-                        + "Menu." + br
-                        + "0. Find items by name." + br
-                        + "1. Exit Program." + br));*/
-
+            Item itemOneExtra = new Item();
+            itemOneExtra.setName(nameOne);
+            tracker.add(itemOneExtra);
+            assertThat(tracker.findByName(nameOne).size(), is(2));
+        }
     }
 
     @Test
